41ba185a6dac174160df6977726e8a135d5ea3e4
TheLastRar <TheLastRar@users.noreply.github.com>
2024-04-17 12:15:44 +0100

    DEV9: Initial support for open ports
    This reverts commit 17aa25600701a67fc16db586cfd2c6e8c9cd0394.

--- pcsx2/Config.h
+++ pcsx2/Config.h
@@ -874,6 +874,13 @@ struct Pcsx2Config
 		};
 		static const char* DnsModeNames[];
 
+		enum struct PortMode : int
+		{
+			UDP = 0,
+			TCP = 1,
+		};
+		static const char* PortModeNames[];
+
 		struct HostEntry
 		{
 			std::string Url;
@@ -883,6 +890,17 @@ struct Pcsx2Config
 
 			bool operator==(const HostEntry& right) const;
 			bool operator!=(const HostEntry& right) const;
+		};
+
+		struct PortEntry
+		{
+			uint Port{};
+			std::string Desc;
+			PortMode Protocol;
+			bool Enabled;
+
+			bool operator==(const PortEntry& right) const;
+			bool operator!=(const PortEntry& right) const;
 		};
 
 		bool EthEnable{false};
@@ -903,6 +921,8 @@ struct Pcsx2Config
 		DnsMode ModeDNS2{DnsMode::Auto};
 
 		std::vector<HostEntry> EthHosts;
+
+		std::vector<PortEntry> OpenPorts;
 
 		bool HddEnable{false};
 		std::string HddFile;
--- pcsx2/Pcsx2Config.cpp
+++ pcsx2/Pcsx2Config.cpp
@@ -1144,6 +1144,11 @@ const char* Pcsx2Config::DEV9Options::DnsModeNames[] = {
 	"Internal",
 	nullptr};
 
+const char* Pcsx2Config::DEV9Options::PortModeNames[] = {
+	"UDP",
+	"TCP",
+	nullptr};
+
 Pcsx2Config::DEV9Options::DEV9Options()
 {
 	HddFile = "DEV9hdd.raw";
@@ -1195,7 +1200,10 @@ void Pcsx2Config::DEV9Options::LoadSave(SettingsWrapper& wrap)
 	}
 
 	if (wrap.IsLoading())
+	{
 		EthHosts.clear();
+		OpenPorts.clear();
+	}
 
 	int hostCount = static_cast<int>(EthHosts.size());
 	{
@@ -1233,6 +1241,30 @@ void Pcsx2Config::DEV9Options::LoadSave(SettingsWrapper& wrap)
 		}
 	}
 
+	int portCount = static_cast<int>(OpenPorts.size());
+	{
+		SettingsWrapSection("DEV9/Eth/Ports");
+		SettingsWrapEntryEx(portCount, "Count");
+	}
+
+	for (int i = 0; i < portCount; i++)
+	{
+		std::string section = "DEV9/Eth/Ports/Port" + std::to_string(i);
+		SettingsWrapSection(section.c_str());
+
+		PortEntry entry;
+		if (wrap.IsSaving())
+			entry = OpenPorts[i];
+
+		SettingsWrapEntryEx(entry.Port, "Port");
+		SettingsWrapEntryEx(entry.Desc, "Desc");
+		SettingsWrapEnumEx(entry.Protocol, "Protocol", PortModeNames);
+		SettingsWrapEntryEx(entry.Enabled, "Enabled");
+
+		if (wrap.IsLoading())
+			OpenPorts.push_back(entry);
+	}
+
 	{
 		SettingsWrapSection("DEV9/Hdd");
 		SettingsWrapEntry(HddEnable);
@@ -1266,6 +1298,8 @@ bool Pcsx2Config::DEV9Options::operator==(const DEV9Options& right) const
 
 		   OpEqu(EthHosts) &&
 
+		   OpEqu(OpenPorts) &&
+
 		   OpEqu(HddEnable) &&
 		   OpEqu(HddFile);
 }
@@ -1291,6 +1325,19 @@ bool Pcsx2Config::DEV9Options::HostEntry::operator==(const HostEntry& right) con
 }
 
 bool Pcsx2Config::DEV9Options::HostEntry::operator!=(const HostEntry& right) const
+{
+	return !this->operator==(right);
+}
+
+bool Pcsx2Config::DEV9Options::PortEntry::operator==(const PortEntry& right) const
+{
+	return OpEqu(Port) &&
+		   OpEqu(Desc) &&
+		   OpEqu(Protocol) &&
+		   OpEqu(Enabled);
+}
+
+bool Pcsx2Config::DEV9Options::PortEntry::operator!=(const PortEntry& right) const
 {
 	return !this->operator==(right);
 }
