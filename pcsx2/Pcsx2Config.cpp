0f0a20df17909fdbb84b1a0ae228ece140d8f65d
TheLastRar <TheLastRar@users.noreply.github.com>
2024-04-17 12:15:44 +0100

    DEV9: Initial support for open ports
    This reverts commit 17aa25600701a67fc16db586cfd2c6e8c9cd0394.

--- pcsx2/Pcsx2Config.cpp
+++ pcsx2/Pcsx2Config.cpp
@@ -1231,6 +1231,11 @@ const char* Pcsx2Config::DEV9Options::DnsModeNames[] = {
 	"Internal",
 	nullptr};
 
+const char* Pcsx2Config::DEV9Options::PortModeNames[] = {
+	"UDP",
+	"TCP",
+	nullptr};
+
 Pcsx2Config::DEV9Options::DEV9Options()
 {
 	HddFile = "DEV9hdd.raw";
@@ -1282,7 +1287,10 @@ void Pcsx2Config::DEV9Options::LoadSave(SettingsWrapper& wrap)
 	}
 
 	if (wrap.IsLoading())
+	{
 		EthHosts.clear();
+		OpenPorts.clear();
+	}
 
 	int hostCount = static_cast<int>(EthHosts.size());
 	{
@@ -1320,6 +1328,30 @@ void Pcsx2Config::DEV9Options::LoadSave(SettingsWrapper& wrap)
 		}
 	}
 
+	int portCount = static_cast<int>(OpenPorts.size());
+	{
+		SettingsWrapSection("DEV9/Eth/Ports");
+		SettingsWrapEntryEx(portCount, "Count");
+	}
+
+	for (int i = 0; i < portCount; i++)
+	{
+		std::string section = "DEV9/Eth/Ports/Port" + std::to_string(i);
+		SettingsWrapSection(section.c_str());
+
+		PortEntry entry;
+		if (wrap.IsSaving())
+			entry = OpenPorts[i];
+
+		SettingsWrapEntryEx(entry.Port, "Port");
+		SettingsWrapEntryEx(entry.Desc, "Desc");
+		SettingsWrapEnumEx(entry.Protocol, "Protocol", PortModeNames);
+		SettingsWrapEntryEx(entry.Enabled, "Enabled");
+
+		if (wrap.IsLoading())
+			OpenPorts.push_back(entry);
+	}
+
 	{
 		SettingsWrapSection("DEV9/Hdd");
 		SettingsWrapEntry(HddEnable);
@@ -1353,6 +1385,8 @@ bool Pcsx2Config::DEV9Options::operator==(const DEV9Options& right) const
 
 		   OpEqu(EthHosts) &&
 
+		   OpEqu(OpenPorts) &&
+
 		   OpEqu(HddEnable) &&
 		   OpEqu(HddFile);
 }
@@ -1378,6 +1412,19 @@ bool Pcsx2Config::DEV9Options::HostEntry::operator==(const HostEntry& right) con
 }
 
 bool Pcsx2Config::DEV9Options::HostEntry::operator!=(const HostEntry& right) const
+{
+	return !this->operator==(right);
+}
+
+bool Pcsx2Config::DEV9Options::PortEntry::operator==(const PortEntry& right) const
+{
+	return OpEqu(Port) &&
+		   OpEqu(Desc) &&
+		   OpEqu(Protocol) &&
+		   OpEqu(Enabled);
+}
+
+bool Pcsx2Config::DEV9Options::PortEntry::operator!=(const PortEntry& right) const
 {
 	return !this->operator==(right);
 }
