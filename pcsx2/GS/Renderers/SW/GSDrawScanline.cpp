5f4cf24b031a60657ebfaf1d7498da25f30a68fe
TheLastRar <TheLastRar@users.noreply.github.com>
2024-08-24 17:38:40 +0100

    GS: ARM64EC compatibility

--- pcsx2/GS/Renderers/SW/GSDrawScanline.cpp
+++ pcsx2/GS/Renderers/SW/GSDrawScanline.cpp
@@ -11,7 +11,9 @@
 #include <fstream>
 
 // Comment to disable all dynamic code generation.
+#if !defined(_M_ARM64EC)
 #define ENABLE_JIT_RASTERIZER
+#endif
 
 #if MULTI_ISA_COMPILE_ONCE
 // Lack of a better home
