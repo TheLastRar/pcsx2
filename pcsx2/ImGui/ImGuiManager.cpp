b0a286432f2b5534e73fadf66593d50fe58d1be3
TheLastRar <TheLastRar@users.noreply.github.com>
2025-03-24 13:32:25 +0000

    Test emojis inline source

--- pcsx2/ImGui/ImGuiManager.cpp
+++ pcsx2/ImGui/ImGuiManager.cpp
@@ -573,6 +573,8 @@ bool ImGuiManager::AddEmojiFont()
 	{
 		ImFontConfig cfg;
 		cfg.MergeMode = true;
+		cfg.GlyphMinAdvanceX = FONT_BASE_SIZE;
+		cfg.GlyphMaxAdvanceX = FONT_BASE_SIZE;
 		cfg.FontBuilderFlags |= ImGuiFreeTypeBuilderFlags_LoadColor;
 		cfg.FontDataOwnedByAtlas = false;
 
