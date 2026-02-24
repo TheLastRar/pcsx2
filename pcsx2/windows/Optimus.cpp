c26bd656fd4c9218bfe027b0c676fb52845441de
TheLastRar <TheLastRar@users.noreply.github.com>
2026-02-24 12:03:29 +0000

    Update Optimus.cpp

--- pcsx2/windows/Optimus.cpp
+++ pcsx2/windows/Optimus.cpp
@@ -4,16 +4,6 @@
 #ifdef _WIN32
 #include "common/RedtapeWindows.h"
 
-//This ensures that the nVidia graphics card is used for PCSX2 on an Optimus-enabled system.
-//302 or higher driver required.
-extern "C" {
-	_declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
-}
 
-//This is the equivalent for an AMD system.
-//13.35 or newer driver required.
-extern "C" {
-	_declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
-}
 
 #endif
