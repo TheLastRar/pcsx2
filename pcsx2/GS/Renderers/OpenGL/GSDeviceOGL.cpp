f46f724c70432b3858e65c2f843048f4a728dc41
TheLastRar <TheLastRar@users.noreply.github.com>
2026-06-04 19:30:27 +0100

    GS/GL: Reset last used program in DoLibrashader()

--- pcsx2/GS/Renderers/OpenGL/GSDeviceOGL.cpp
+++ pcsx2/GS/Renderers/OpenGL/GSDeviceOGL.cpp
@@ -3422,11 +3422,13 @@ bool GSDeviceOGL::DoLibrashader(GSTexture* sTex, GSTexture* dTex)
 	{
 		libra_error_free(&err);
 		GLState::Invalidate();
+		GLProgram::ResetLastProgram();
 		return false;
 	}
 
 	// librashader binds its own program/FBO/samplers wipe our cache so the next call rebinds.
 	GLState::Invalidate();
+	GLProgram::ResetLastProgram();
 	return true;
 }
 
